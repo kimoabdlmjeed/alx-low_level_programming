52047bb4984839a0447a7c1645b5d608cc5285bf
kimoabdlmjeed <kimo200160@gmail.com>
2023-07-18 10:03:20 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+
+int main(void)
+{
+char w[9] = "_putchar";
+int counter = 0;
+for (counter = 0 ; counter < 8 ; counter++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
+}
